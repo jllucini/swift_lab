7fccc630e9d8c6994195d740a714273a5332a384
Jose Luis Lucini <jllucini@gmail.com>
2015-10-04 21:16:25 +0200

    Redo changes, error with OS El Capitan

--- Trax/GPXViewController.swift
+++ Trax/GPXViewController.swift
@@ -13,7 +13,7 @@ import UIKit
 import MapKit
 
 class GPXViewController: UIViewController, MKMapViewDelegate {
-
+    
     @IBOutlet weak var mapView: MKMapView!{
         didSet {
             mapView.mapType  = .Satellite
@@ -49,6 +49,8 @@ class GPXViewController: UIViewController, MKMapViewDelegate {
                 self.gpxURL = url
             }
         }
+        
+        gpxURL = NSURL(string: "http://cs193p.stanford.edu/Vacation.gpx") // for demo/debug/testing
     }
     
     // MARK: - View Aux Functions
@@ -64,8 +66,90 @@ class GPXViewController: UIViewController, MKMapViewDelegate {
         mapView.showAnnotations(waypoints, animated: true)
     }
     
-    // MARK: - MKMapViewDelegate
+    // MARK: - Navigation
     
-
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == Constants.ShowImageSegue {
+            if let waypoint = (sender as? MKAnnotationView)?.annotation as? GPX.Waypoint {
+                if let ivc = segue.destinationViewController.contentViewController as? ImageViewController {
+                    ivc.imageURL = waypoint.imageURL
+                    ivc.title    = waypoint.name
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - MKMapViewDelegate
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var view = mapView.dequeueReusableAnnotationViewWithIdentifier(Constants.AnnotationViewReuseIdentifier)
+        
+        if view == nil {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: Constants.AnnotationViewReuseIdentifier)
+            view.canShowCallout = true
+        } else {
+            view.annotation = annotation
+            view.canShowCallout = true
+        }
+        
+        view.leftCalloutAccessoryView  = nil
+        view.rightCalloutAccessoryView = nil
+        if let waypoint = annotation as? GPX.Waypoint {
+            if waypoint.thumbnailURL != nil {
+                view.leftCalloutAccessoryView = UIImageView(frame: Constants.LeftCalloutFrame)
+                // Don't fetch the image here: Load the image in another delegate
+                // (mapview:didSelectAnotationView)
+                // In this method we are simly creating the pin and the assocaited callout view.
+            }
+            if waypoint.imageURL != nil {
+                view.rightCalloutAccessoryView = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as UIButton
+            }
+        }
+        
+        return view
+    }
+    
+    
+    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
+        if let waypoint = view.annotation as? GPX.Waypoint {
+            if let thumbnailImageView = view.leftCalloutAccessoryView as? UIImageView {
+                if let url = waypoint.thumbnailURL {
+                    let qos = Int(QOS_CLASS_USER_INTERACTIVE.value)
+                    dispatch_async(dispatch_get_global_queue(qos, 0)) {
+                        // Fetch the image async
+                        if let imageData = NSData(contentsOfURL: url) {
+                            // Go back to main queue and set the imageData
+                            dispatch_async(dispatch_get_main_queue()) {
+                                if url == waypoint.thumbnailURL!{
+                                    if let image = UIImage(data: imageData) {
+                                        thumbnailImageView.image = image
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+        performSegueWithIdentifier(Constants.ShowImageSegue, sender: view)
+        // Need to prepareFroSegue
+        // Copy from "Cassini" project ImageViewController's class. Also copy the assocaited VC from the Storyboard
+        // Do segue from GPXViewController to new VC
+        // Also embed in Navigation Controller
+    }
+    
+    // MARK: - Constants
+    
+    private struct Constants {
+        static let LeftCalloutFrame = CGRect(x: 0, y: 0, width: 59, height: 59)
+        static let AnnotationViewReuseIdentifier = "waypoint"
+        static let ShowImageSegue = "Show Image"
+        static let EditWaypointSegue = "Edit Waypoint"
+        static let EditWaypointPopoverWidth: CGFloat = 320
+    }
+    
 }
 
